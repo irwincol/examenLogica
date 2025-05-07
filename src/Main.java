03030803c721d2ff8a97ee71ce0a8302b3ee91fe
irwincol <irwincolmenarez10@gmail.com>
2025-05-07 18:29:15 -0500

    inicio examen de logica

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        Contextualización: La tienda GameWorld vende productos y ofrece servicios especiales a sus clientes.
+//        El programa debe permitir seleccionar entre varias opciones para calcular descuentos o beneficios.
+//                Menú de opciones:
+//        1. Comprar videojuego
+//        2. Comprar accesorio
+//        3. Solicitar membresía
+//        Condiciones:
+//        Opción 1 - Comprar videojuego: Solicitar el valor del juego y si el cliente es recurrente.
+//• Si el valor del juego es mayor a $150000, aplicar 10% de descuento.
+//• Si es cliente recurrente, aplicar 5% adicional.
+//                Opción 2 - Comprar accesorio: Solicitar tipo de accesorio y su valor.
+//• Si el valor supera $200000, aplicar 8% de descuento.
+//• Si el valor está entre $50000 y $200000, aplicar 5% de descuento.
+//                Opción 3 - Solicitar membresía: Solicitar edad del cliente.
+//• Menores de 18 años no pueden solicitar.
+//• Entre 18 y 49 años pueden solicitar membresía estándar.
+//• A partir de 50 años obtienen membresía premium.
+//        Resultados esperados: Mostrar opción elegida, datos ingresados y resultado final.
+//
+        Scanner sc = new Scanner(System.in);
+    }
+}
